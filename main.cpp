8b2efe0db8b866e5b7b7a1e7ce712d548794e3f4
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-10-02 09:19:31 +0300

    canot create stackonly class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <new>
 #include <memory>
+#include <cstdlib>
 
-struct StackOnly
+struct StackOnly 
 {
 	void* operator new (std::size_t size) = delete;
 	void* operator new (std::size_t size, const std::nothrow_t& nothrow_value) noexcept = delete;
@@ -27,7 +28,15 @@ int main()
 {
 	StackOnly stack_only;
 //	auto error1 = new StackOnly;
+
+	// hack for allocate StackOnly on the heap
+	StackOnly *ptr = static_cast<StackOnly*>(std::malloc(sizeof(StackOnly)));
+	// but still cannot call constructor
+//	auto error2 = new (ptr) StackOnly();
+	ptr->~StackOnly();
+	std::free(ptr);
+
 	auto heap_only = HeapOnly::create();
-//	HeapOnly error2;
+//	HeapOnly error3;
 }
 
